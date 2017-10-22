e6b81dd1ac5dbbc6c3fdc2b42b31f160ee06307b
Miau Lightouch <miaulightouch@gmail.com>
2017-10-22 17:37:13 +0800

    update preload script

--- static/preload/preload.js
+++ static/preload/preload.js
@@ -1,38 +1,80 @@
 'use strict'
 
 ;(() => {
+  var oldconsolelog = window.console.log
+  var oldconsoleerr = window.console.error
+  var oldconsolewarn = window.console.warn
+  eventInjector()
   window.addEventListener('DOMContentLoaded', () => {
+    consoleRecover()
     cssOverride()
     windowResizer()
     // windowResizeInject()
   })
 
+  /**
+   * Inject addEventListener to clear mute event
+   */
+  function eventInjector () {
+    let _addEventListener = window.addEventListener
+    window.addEventListener = function (a, b, c) {
+      let func = b.toString()
+      let blockReg = /mute/
+      let whiteReg = /unmute/
+      if (process.env.NODE_ENV === 'development') {
+        if (!whiteReg.test(func)) {
+          if (blockReg.test(b.toString())) {
+            if (!this.eventListenerBlockedList) this.eventListenerBlockedList = {}
+            if (!this.eventListenerBlockedList[a]) this.eventListenerBlockedList[a] = []
+            this.eventListenerBlockedList[a].push(b)
+            return
+          }
+        }
+      }
+      _addEventListener(a, b, c)
+    }
+  }
+
+  /**
+   * Take back the console.log
+   */
+  function consoleRecover () {
+    if (process.env.NODE_ENV === 'development') {
+      window.console.log = oldconsolelog
+      window.console.error = oldconsoleerr
+      window.console.warn = oldconsolewarn
+    }
+  }
+
+  /**
+   * Inject css rule to head
+   */
   function cssOverride () {
-    /**
-     * Inject css rule to head
-     */
     let cssContent = require('fs').readFileSync(require('path').join(__dirname, 'override.css'), 'utf8')
     let cssOverride = document.createElement('style')
     cssOverride.appendChild(document.createTextNode(cssContent))
     document.head.appendChild(cssOverride)
   }
 
+  /**
+   * override window resizer
+   */
   function windowResizer () {
-    /**
-     * override window resizer
-     */
-    function zoomSetter () {
+    let zoomSetter = () => {
       document.getElementById(window.Game.gameContainer.id).style.zoom = window.innerWidth / 320
     }
-    setTimeout(() => zoomSetter, 100)
-    window.onresize = () => zoomSetter
+    let fastResizer = setInterval(zoomSetter, 0)
+    setTimeout(() => {
+      clearInterval(fastResizer)
+    }, 1000)
+    window.onresize = zoomSetter
   }
 
-  function windowResizeInject () {
-    /**
-     * Make size button can change window size
-     * FIXME: NOT IMPLEMENT
-     */
+  /**
+   * Make size button can change window size
+   * FIXME: NOT IMPLEMENT
+   */
+  function windowControllInject () {
     let resizeBtn = document.getElementsByClassName('btn-pc-footer-setting')
     for (let index in resizeBtn) {
       if (resizeBtn.hasOwnProperty(index)) {
