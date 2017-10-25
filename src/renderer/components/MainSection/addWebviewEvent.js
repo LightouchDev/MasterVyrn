9b2bb0cdf95ebb69511ed1b5513914556d60c170
Miau Lightouch <miaulightouch@gmail.com>
2017-10-25 20:11:33 +0800

    adjust execute order

--- src/renderer/components/MainSection/addWebviewEvent.js
+++ src/renderer/components/MainSection/addWebviewEvent.js
@@ -1,13 +1,17 @@
+'use strict'
+
 function addWebviewEvent () {
   window.addEventListener('DOMContentLoaded', () => {
     const webview = document.querySelector('webview')
-    webview.addEventListener('dom-ready', (evt) => {
+    webview.addEventListener('dom-ready', () => {
       if (process.env.NODE_ENV === 'development') {
         console.log('WEBVIEW READY!')
         webview.openDevTools()
       }
+    })
+    webview.addEventListener('did-navigate', (evt) => {
       window.vue.$store.commit('CHANGE_URL', {
-        'url': webview.getURL()
+        'url': evt.url
       })
     })
     webview.addEventListener('did-navigate-in-page', (evt) => {
