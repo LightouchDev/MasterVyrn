5fa18d384d9aa6c5521bdf04113aae34aaca45f9
Miau Lightouch <miaulightouch@gmail.com>
2017-10-30 10:54:51 +0800

    move injector into webview to solve performance issue

--- src/main/proxy/rules.js
+++ src/main/proxy/rules.js
@@ -1,29 +1,15 @@
-import {URL} from 'url'
-import {JSDOM} from 'jsdom'
+// import {URL} from 'url'
 
 // FIXME: no hardcoded rule, use object instead.
 export default {
-  summary: 'GBF resource process',
+  summary: 'GBF resource monitor',
   * beforeSendResponse (requestDetail, responseDetail) {
+    /*
     let url = new URL(requestDetail.url)
     if (url.hostname === 'game.granbluefantasy.jp') {
-      let newResponse = Object.assign({}, responseDetail.response)
-      if (url.pathname === '/' || url.pathname === '/maintain') {
-        let {document} = new JSDOM(responseDetail.response.body, {resources: 'usable'}).window
-        let elements = document.head.querySelectorAll('script')
-        for (let element of elements) {
-          if (!element.src) {
-            element.text = element.text.replace(/^[ \t]+deviceRatio.*\n/gm, '')
-          }
-        }
-        let cssContent = require('fs').readFileSync(require('path').join(__dirname, 'resources/override.css'), 'utf8')
-        let cssOverride = document.createElement('style')
-        cssOverride.appendChild(document.createTextNode(cssContent))
-        document.head.appendChild(cssOverride)
-        Object.assign(newResponse, {body: document.documentElement.innerHTML})
-        return {response: newResponse}
-      }
+      // some data listener here
     }
+    */
     return null // keep response clean if target not found.
   }
 }
--- static/preload/index.js
+++ static/preload/index.js
@@ -2,15 +2,21 @@
 'use strict'
 
 ;(() => {
+  let startTime = new Date().getTime()
   devtronDeps()
+  DOMWatcher()
   var oldconsolelog = window.console.log
   var oldconsoleerr = window.console.error
   var oldconsolewarn = window.console.warn
   eventInjector()
   window.addEventListener('DOMContentLoaded', () => {
+    oldconsolelog(new Date().getTime() - startTime, 'DOM parsed')
     consoleRecover()
     windowResizer()
     // windowResizeInject()
+  })
+  window.addEventListener('load', () => {
+    oldconsolelog(new Date().getTime() - startTime, 'DOM ready')
   })
 
   /**
@@ -20,6 +26,43 @@
     if (process.env.NODE_ENV === 'development') {
       window.__devtron = {require: require, process: process}
     }
+  }
+
+  /**
+   * Watch and patch DOMs
+   */
+  function DOMWatcher () {
+    const startTime = new Date().getTime()
+    let cssContent = require('fs').readFileSync(require('path').join(__dirname, 'override.css'), 'utf8')
+    const htmlWatcher = new window.MutationObserver(mutation => {
+      console.warn(new Date().getTime() - startTime, 'start looking for head!')
+      if (document.head) {
+        console.warn(new Date().getTime() - startTime, 'head detected!')
+        headWatcher.observe(document.head, config)
+        let cssOverride = document.createElement('style')
+        cssOverride.appendChild(document.createTextNode(cssContent))
+        document.head.appendChild(cssOverride)
+        htmlWatcher.disconnect()
+      }
+    })
+    const headWatcher = new window.MutationObserver(mutations => {
+      let regex = /^[ \t]+deviceRatio.*\n/gm
+      for (let mutation of mutations) {
+        if (mutation.addedNodes) {
+          for (let element of mutation.addedNodes) {
+            if (element.nodeName === 'SCRIPT') {
+              if (regex.test(element.text)) {
+                element.text = element.text.replace(regex, '')
+                console.warn(new Date().getTime() - startTime, 'target detected!')
+                headWatcher.disconnect()
+              }
+            }
+          }
+        }
+      }
+    })
+    const config = { childList: true, subtree: true }
+    htmlWatcher.observe(document, config)
   }
 
   /**
@@ -61,7 +104,7 @@
    */
   function windowResizer () {
     window.onresize = () => {
-      document.getElementById(window.Game.gameContainer.id).style.zoom = window.displayInitialize()
+      document.location.reload()
     }
   }
 
