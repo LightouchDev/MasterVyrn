8a44ae66be4f2024230721158253169f99dab80d
Miau Lightouch <miaulightouch@gmail.com>
2017-11-02 19:29:36 +0800

    Fix window too tiny and cause overflow in Windows

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,11 +11,7 @@
 </script>
 
 <style lang="scss">
-html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    font-family: $defaultFont;
+#app {
+  overflow: hidden;
 }
 </style>
