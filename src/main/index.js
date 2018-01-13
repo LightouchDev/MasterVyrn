bc5b6e1c021b7efe0977a9b3b1c941cc6748d8f6
Miau Lightouch <miaulightouch@gmail.com>
2018-01-14 04:10:11 +0800

    Fix spell-check issue

--- src/main/index.js
+++ src/main/index.js
@@ -77,7 +77,7 @@ process.on('uncaughtException', printError)
 /**
  * Auto Updater
  *
- * Uncomment the following code below and install `electron-updater` to
+ * Un-comment the following code below and install `electron-updater` to
  * support auto updating. Code Signing with a valid certificate is required.
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
