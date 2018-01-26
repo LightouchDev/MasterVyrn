bc8fca7969c849b0e086ada420906b5504892a09
Miau Lightouch <miaulightouch@gmail.com>
2018-01-26 08:19:29 +0800

    Improve hotkey logic

--- src/renderer/libs/registerHotkey.js
+++ src/renderer/libs/registerHotkey.js
@@ -13,7 +13,22 @@ const keyTable = {
   Super: 'metaKey'
 }
 
-const registeredSet = []
+const registeredSet = {}
+
+function concatKeys (keyObject, lastKey) {
+  let key = ''
+  ;['ctrlKey', 'metaKey', 'altKey', 'shiftKey'].forEach(modifier => {
+    if (keyObject[modifier]) {
+      if (key) key += '+'
+      key += modifier
+    }
+  })
+  if (lastKey) {
+    if (key) key += '+'
+    key += lastKey
+  }
+  return key
+}
 
 /**
  * Parse and register hotkey
@@ -22,56 +37,41 @@ const registeredSet = []
  * @param {Function} callback
  */
 function registerHotkey (hotkeys, callback) {
-  const hotkeyGroup = { callback }
+  const modifierGroup = {}
+  let lastKey = ''
   hotkeys.split('+').forEach(key => {
     if (key === 'CommandOrControl' || key === 'CmdOrCtrl') {
       process.platform === 'darwin'
         ? key = 'Command'
         : key = 'Control'
     }
-    keyTable[key] && (hotkeyGroup[keyTable[key]] = true)
+    keyTable[key] && (modifierGroup[keyTable[key]] = true)
     // test A-Z, 0-9
     if (/^[\dA-Z]$/.test(key)) {
-      hotkeyGroup[key] = true
+      lastKey = key
     } else {
       // test F1 - F12
       let parseTemp = /^F(\d+)$/.exec(key)
       if (parseTemp) {
         parseTemp = Number(parseTemp[1])
         if (parseTemp > 0 && parseTemp < 13) {
-          hotkeyGroup[key] = true
+          lastKey = key
         }
       }
     }
   })
-  registeredSet.push(hotkeyGroup)
+  registeredSet[concatKeys(modifierGroup, lastKey)] = callback
 }
 
 // Register on keyup event
 registerHotkey.startListen = () => {
   log('hotkeys: %o', registeredSet)
   window.onkeydown = function (event) {
-    log(
-      'ctrl: %s, alt: %s, meta: %s, shift: %s, key: %s',
-      event.ctrlKey, event.altKey, event.metaKey, event.shiftKey, event.key
-    )
+    const keys = concatKeys(event, event.key.toUpperCase())
+    log('concat result: %s', keys)
     // Ensure single hotkey do not affect input box
-    if (event.target.nodeName !== 'INPUT' && event.target.nodeName !== 'SELECT') {
-      registeredSet.some(hotkey => {
-        const result = ['ctrlKey', 'altKey', 'metaKey', 'shiftKey'].every(modifier => {
-          return event[modifier] === !!hotkey[modifier]
-        })
-        if (result) {
-          // test A-Z, 0-9, F1 - F12
-          if (/^.$/.test(event.key) || /^F(\d+)$/.test(event.key)) {
-            if (hotkey[event.key.toUpperCase()] === true) {
-              hotkey.callback()
-              return true
-            }
-          }
-        }
-        return false
-      })
+    if (event.target.nodeName !== 'INPUT' && event.target.nodeName !== 'SELECT' && registeredSet[keys]) {
+      registeredSet[keys]()
     }
   }
 }
