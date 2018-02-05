6a83f8a8fc35e8d11367fb556a73533580ff712b
Miau Lightouch <miaulightouch@gmail.com>
2018-02-06 06:26:19 +0800

    Add essential hotkeys

--- src/renderer/components/DefaultApplication/webContentsSetup.js
+++ src/renderer/components/DefaultApplication/webContentsSetup.js
@@ -9,13 +9,33 @@ function webviewDevTools () {
   window.webview.openDevTools({mode: 'detach'})
 }
 
+function webviewRefresh () {
+  window.webview.reload()
+}
+
+function webviewRefreshIgnoringCache () {
+  window.webview.reloadIgnoringCache()
+}
+
 // Set hotkey
 if (process.platform === 'darwin') {
   // Command + Option + I: open game view DevTools on OSX
   registerHotkey('Command+Option+I', webviewDevTools)
+  // Command + R: refresh game view on OSX
+  registerHotkey('Command+R', webviewRefresh)
+  // Command + Shift + R: refresh game view and ignores cache on OSX
+  registerHotkey('Command+Shift+R', webviewRefreshIgnoringCache)
 } else {
   // F12: open game view DevTools
   registerHotkey('F12', webviewDevTools)
+  // F5: refresh game view
+  registerHotkey('F5', webviewRefresh)
+  // Ctrl + R: refresh game view
+  registerHotkey('Ctrl+R', webviewRefresh)
+  // Shift + F5: refresh game view and ignores cache
+  registerHotkey('Shift+F5', webviewRefreshIgnoringCache)
+  // Ctrl + Shift + R: refresh game view and ignores cache
+  registerHotkey('Ctrl+Shift+R', webviewRefreshIgnoringCache)
 }
 
 // Ctrl + Alt + I: open host view DevTools
@@ -37,5 +57,4 @@ registerHotkey('H', () => {
   }
 })
 
-// hook to window.onkeyup
 registerHotkey.startListen()
