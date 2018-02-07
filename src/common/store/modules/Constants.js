61cb0782686208565bf534c1a2ede4bd41d9d5f5
Miau Lightouch <miaulightouch@gmail.com>
2018-02-07 17:10:10 +0800

    Fix preload path issue

--- src/common/store/modules/Constants.js
+++ src/common/store/modules/Constants.js
@@ -4,7 +4,7 @@ const state = {
 
 state.preloadPath = process.mainModule.filename.indexOf('app.asar') !== -1
   ? `file://${require('path').resolve(__dirname, 'preload.js')}`
-  : `file://${require('path').resolve(__dirname, '../../../dist/electron/preload.js')}`
+  : `file://${require('path').resolve(__dirname, '../../../../dist/electron/preload.js')}`
 
 export default {
   state
